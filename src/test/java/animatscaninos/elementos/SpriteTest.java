2a8f3ac3f5806eb069261e57ed70f25dfc6ad8f5
Diego Hernández <diego.hernandez@encora.com>
2023-04-15 20:39:43 -0600

    Se mueven los tests de getDistancia hacia la clase SpriteTest.

--- src/test/java/animatscaninos/elementos/SpriteTest.java
+++ src/test/java/animatscaninos/elementos/SpriteTest.java
@@ -32,8 +32,33 @@ public class SpriteTest {
 
     @ParameterizedTest
     @MethodSource
-    public void testGetAngulo(double anguloEsperado, double x, double y) {
+    void testGetAngulo(double anguloEsperado, double x, double y) {
         assertEquals(anguloEsperado, SPRITE_PRUEBA.getAngulo(x, y),
+                0.0000000000005);
+    }
+
+    static Stream<Arguments> testGetDistancia() {
+        return Stream.of(
+                Arguments.of(0.0, X_PRUEBA, Y_PRUEBA),
+                Arguments.of(10.0, X_PRUEBA - 10, Y_PRUEBA),
+                Arguments.of(10.0, X_PRUEBA, Y_PRUEBA - 10),
+                Arguments.of(10.0, X_PRUEBA + 10, Y_PRUEBA),
+                Arguments.of(10.0, X_PRUEBA, Y_PRUEBA + 10),
+                Arguments.of(5.0, X_PRUEBA - 3, Y_PRUEBA - 4),
+                Arguments.of(5.0, X_PRUEBA + 3, Y_PRUEBA + 4),
+                Arguments.of(5.0, X_PRUEBA + 3, Y_PRUEBA - 4),
+                Arguments.of(5.0, X_PRUEBA - 3, Y_PRUEBA + 4),
+                Arguments.of(6.71394072062004, X_PRUEBA - 5.23, Y_PRUEBA - 4.21),
+                Arguments.of(8.15309143822146, X_PRUEBA + 3.15, Y_PRUEBA + 7.52),
+                Arguments.of(10.2885664696303, X_PRUEBA - 0.89, Y_PRUEBA + 10.25),
+                Arguments.of(67.3054425733908, X_PRUEBA + 23.01, Y_PRUEBA - 63.25)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testGetDistancia(double distanciaEsperada, double x, double y) {
+        assertEquals(distanciaEsperada, SPRITE_PRUEBA.getDistancia(x, y),
                 0.0000000000005);
     }
 }
